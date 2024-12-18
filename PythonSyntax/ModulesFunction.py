5eb6de5eff179802168b3e8b7658c99da92e5f78
nam135792002 <npnam@HSVINA.LOCAL>
2024-12-18 11:50:16 +0700

    coding: module in python

---
+++ PythonSyntax/ModulesFunction.py
@@ -0,0 +1,25 @@
+# import math
+# import math as m
+# from math import e
+
+# a, b, c, d, e = 1, 2, 3, 4, 5
+
+# print(math.e ** a)
+# print(math.e ** b)
+# print(math.e ** c)
+# print(math.e ** d)
+# print(math.e ** e)
+
+pi = 3.14159
+
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def circumference(radius):
+    return 2 * pi * radius
+
+def area(radius):
+    return pi * radius ** 2
---
+++ PythonSyntax/ModulesMain.py
@@ -0,0 +1,9 @@
+import ModulesFunction as mf
+
+result = mf.pi
+print(result)
+
+print(mf.square(3))
+print(mf.cube(3))
+print(mf.circumference(3))
+print(mf.area(3))
