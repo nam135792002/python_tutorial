83d0cdbc03e7cc1a89f0a1621619e2d73fd0cb37
nam135792002 <npnam@HSVINA.LOCAL>
2024-12-18 09:46:43 +0700

    coding: iterable in python

---
+++ PythonSyntax/Iterables.py
@@ -0,0 +1,16 @@
+# numbers = [1, 2, 3, 4, 5]
+
+# for number in reversed(numbers):
+#     print(number, end=" - ")
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+# for fruit in fruits:
+#     print(fruit)
+
+# name = "Nguyen Phuong Nam"
+# for character in name:
+#     print(character, end=" ")
+
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+for key, value in my_dictionary.items():
+    print(f"{key} - {value}")
